199c4a30773c88c07e546c4959858e7a10c18869
yangxule <yangxule@cnstrong.cn>
2020-12-15 11:05:14 +0800

    useContext使用及性能优化

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import React, {
     useReducer
 } from 'react'
 
-const themes = {
+const Themes = {
     light: {
         foreground: "#000000",
         background: "#eeeeee"
@@ -22,12 +22,27 @@ const themes = {
     }
 };
 
-const ThemeContext = React.createContext(themes.light);
+const ThemeContext = React.createContext(Themes);
 
 function App() {
+    const [themes,setThemes] = useState(Themes)
+    const handleClick = ()=>{
+        setThemes((prethemes)=>{
+            return {
+                ...prethemes,
+                red: {
+                    foreground: "#ffffff",
+                    background: "red"
+                }
+            }
+        })
+    }
     return (
-        <ThemeContext.Provider value={themes.dark}>
+        <ThemeContext.Provider value={themes}>
             <Toolbar />
+            <button onClick={handleClick}>
+                改变context的value
+            </button>
         </ThemeContext.Provider>
     );
 }
@@ -41,12 +56,25 @@ function Toolbar(props) {
 }
 
 function ThemedButton() {
-    const theme = useContext(ThemeContext);
-    return (
-        <button style={{ background: theme.background, color: theme.foreground }}>
+    const themes = useContext(ThemeContext);
+    const light = themes.light
+    // return (
+    //     <button style={{ background: light.background, color: light.foreground }}>
+    //         {console.log('render')}
+    //         {console.log(themes,'themes')}
+    //         I am styled by theme context!
+    //     </button>
+    // );
+    console.log(themes,'themes')
+    //通过useMemo的监听及缓存使context在局部改变时，不再重新渲染使用useContext的组件。
+    //这对于高开销组件的缓存很有用
+    //memo只会对props做潜比较，所以在使用useContext的组件中，即使组件使用了memo，当context局部改变时，也会重新渲染组件。
+    return useMemo(()=>(
+        <button style={{ background: light.background, color: light.foreground }}>
+            {console.log('render')}
             I am styled by theme context!
         </button>
-    );
+    ),[light])
 }
 
 export default App;
