09c6b8abcc32eea1e3a32b8a8763c63d4854ab3b
yangxule <yangxule@cnstrong.cn>
2021-01-26 17:33:43 +0800

    TS高级技巧

--- src/App.js
+++ src/App.js
@@ -16,10 +16,6 @@ import React, {
 
 import Ts from './Ts'
 
-/**
- * TS环境置入：npm install --save typescript @types/node @types/react @types/react-dom @types/jest
- * via-https://juejin.cn/post/6844903911396999182
- */
 
 const App = ()=>{
 
--- src/Ts.tsx
+++ src/Ts.tsx
@@ -13,6 +13,38 @@ import React, {
     useDebugValue
 } from 'react'
 
+/*
+TypeScript 中提升幸福感的 10 个高级技巧
+via-https://juejin.cn/post/6919478002925453320
+*/
+
+type DeepReadonly<T> = {
+    readonly [P in keyof T]: DeepReadonly<T[P]>;
+}
+
+const a = {
+    foo: { bar: 22 },
+    baz: { hah: 'yang'}
+
+}
+const b = a as DeepReadonly<typeof a>
+b.foo.bar = 33
+b.baz.hah = 22
+
+interface SetPoint {
+    (x:number, y: number): void;
+}
+
+// type SetPoint = (x: number, y?: number)=>void;
+
+const setPoint: SetPoint = (x, y)=>{
+
+}
+
+function setPoint1 (x: number, y: number): void{
+
+}
+
 
 const Ts = ()=>{
     return (
